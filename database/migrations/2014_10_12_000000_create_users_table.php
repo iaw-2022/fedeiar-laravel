656d86c9b02f6fa421b6ec71c8c6574d72e16e25
unknown <iarlorifederico@gmail.com>
2022-03-29 21:59:12 -0300

    actualización tabla usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,13 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            //$table->id();
+            $table->string('name')->primary();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('nacionalidad');
+            $table->string('rol');
             $table->rememberToken();
             $table->timestamps();
         });
