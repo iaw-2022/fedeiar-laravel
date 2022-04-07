29b8fa4cdc763608f526e0ef89926b00014d2664
Federico <iarlorifederico@gmail.com>
2022-04-07 20:26:27 -0300

    tablas

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,20 +18,6 @@ class User extends Authenticatable
     protected $table = 'users';
 
     /**
-     * Indicates if the model's ID is auto-incrementing.
-     *
-     * @var bool
-     */
-    public $incrementing = false;
-
-    /**
-     * The data type of the auto-incrementing ID.
-     *
-     * @var string
-     */
-    protected $keyType = 'string';
-
-    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', function () {
+*/
+Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
